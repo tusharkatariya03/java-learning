f4fa90624d8f642c9be7941338d263d74ab84d36
tusharkatariya03 <tusharkatariya0325@gmail.com>
2026-07-07 21:42:24 +0530

    day 01-Hello world

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args ) {
         System.out.println("Hello World");
+//        System.out.print("Hello World");
+          System.out.print("tushar");
     }
 }
